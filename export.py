4a3c35eeb6f36e3cc45d5afc7994998e2ab12cbd
Ian Barton <ian@manor-farm.org>
2017-10-31 18:12:17 +0000

    Some utf8 fixes.
    Make sure it works with Python 3.

--- export.py
+++ export.py
@@ -4,11 +4,11 @@ import listparser as lp
 d = lp.parse("podcasts_opml.xml")
 
 f = open('podcasts.org', 'w')
-f.write('|-|-|\n')
-f.write('|url|Title|\n')
-f.write('|-|-|\n')
+f.write("|-|-|\n")
+f.write("|url|Title|\n")
+f.write("|-|-|\n")
 for podcast in d.feeds:
-    f.write('|%s| %s|\n' % (podcast.url.encode('utf8'), podcast.title.encode('utf8')))
-f.write('|-|-|\n')
+    f.write("|%s| %s|\n" % (podcast.url, podcast.title))
+f.write("|-|-|\n")
 
 f.close()
